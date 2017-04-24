31adc38ffafa65509158069762fe9eea4bfbeb19
mrmckain <mrmckain@gmail.com>
2017-04-24 00:04:04 -0500

    Ignore N's in assembly for coverage since they are from scaffolds.

--- Coverage_Analysis/check_plastid_coverage.pl
+++ Coverage_Analysis/check_plastid_coverage.pl
@@ -6,23 +6,40 @@ use strict;
 ####Usage: 1-Coverage file 2-Kmer Size 3-Coverage Min
 
 my $kmer=$ARGV[1];
-my $mincov=$ARGV[2];
+my $mincov;
+if($ARGV[2]){
+	$mincov=$ARGV[2];
+}
 my $exp_id;
 $ARGV[0] =~ /(.*?).coverage_25kmer.txt/;
 $exp_id=$1;
 
+open my $file, "<", $ARGV[0];
+my $total_cov;
+my $total_windows;
+while(<$file>){
+        chomp;
+        my @tarray=split/\s+/;
+	$total_cov +=$tarray[2];
+	$total_windows++;
+}
+
+my $avg_cov = $total_cov/$total_windows;
+unless($mincov){
+	$mincov = ($avg_cov/$total_windows)*0.25;
+}
 my $current_kmer=0;
 my $current_kmer_start;
 my $current_kmer_end;
 
 
 open my $out, ">", $exp_id  . "_problem_regions_plastid_assembly.txt";
-open my $file, "<", $ARGV[0];
+open $file, "<", $ARGV[0];
 while(<$file>){
 	chomp;
 	my @tarray=split/\s+/;
 	
-	if($tarray[2] <= $mincov){
+	if($tarray[2] <= $mincov && $tarray[0] !~ /N/i){
 		if($current_kmer_start){
 			$current_kmer_end=$tarray[1];
 			$current_kmer++;
